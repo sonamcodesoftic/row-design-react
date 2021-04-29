81b6e3a126a627e6a1aff5b32de927525e86f82a
himanshucodesoftic <himanshudhiman@codesoftic.com>
2021-04-29 15:44:36 +0530

    message

--- src/Component/Header/Headerpage.js
+++ src/Component/Header/Headerpage.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import Routespage from '../../Routes/Routespage';
-
+// import Routespage from '../../Routes/Routespage';
 
 const Headerpage = () => {
     return(
         <>
-        <Router>
+        {/* <Router> */}
         <div class=""> 
         <nav class="navbar navbar-expand-md bg-dark navbar-dark navbar-position">
             <Link class="navbar-brand" to = "/">Navbar</Link>
@@ -17,24 +16,24 @@ const Headerpage = () => {
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav blog-item">
       <li class="nav-item">
-        <Link to = "/Bloghomepage" class="nav-link "> <u> BLOG </u></Link>
+        <Link to = "/admin/Bloghomepage" class="nav-link "> <u> BLOG </u></Link>
         
         {/* <a class="nav-link blog-item" href="#">BLOG</a> */}
       </li> 
       <li class="nav-item">
-        <Link to = "/Loginpage" class="nav-link "><u> LOGIN </u></Link>
+        <Link to = "/admin/Loginpage" class="nav-link "><u> LOGIN </u></Link>
         {/* <a class="nav-link blog-item" href="#">BLOG</a> */}
       </li>
-      <li class="nav-item">
-        <Link to = "/Sidebarpage" class="nav-link "><u> Dashboard </u></Link>
-        {/* <a class="nav-link blog-item" href="#">BLOG</a> */}
-      </li>
+      {/* <li class="nav-item">
+        <Link to = "/admin/Sidebarpage" class="nav-link "><u> Dashboard </u></Link>
+        <a class="nav-link blog-item" href="#">BLOG</a>
+      </li> */}
     </ul>
   </div>  
 </nav>
         </div>
-        <Routespage />
-        </Router>
+         {/* <Routespage /> 
+        </Router> */}
         </>
     );
 }
